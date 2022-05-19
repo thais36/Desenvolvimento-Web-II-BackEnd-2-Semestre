363e0c27930fdd26be9f03e4877479985d367b4d
Thais Almeida <90707459+thais36@users.noreply.github.com>
2022-05-19 18:36:34 -0300

    Delete config.bat

--- config.bat
+++
@@ -1,8 +0,0 @@
-npm install express --save
-npm install nodemon -g
-npm install sha1
-npm install mysql2
-npm install body-parser
-npm install body-parsernpm install ejs
-npm install consign
-npm install passport express-session
